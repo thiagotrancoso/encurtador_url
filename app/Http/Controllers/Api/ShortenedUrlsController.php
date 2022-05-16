38955011e2fcc247f80205ce9d4fa57a970023c0
Thiago Trancoso <thiagostrn@gmail.com>
2022-05-16 14:56:20 -0300

    generated shortened url

---
+++ app/Http/Controllers/Api/ShortenedUrlsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Service\ShortenedUrlService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ShortenedUrlsController extends Controller
+{
+    private ShortenedUrlService $shortenedUrlService;
+
+    public function __construct(ShortenedUrlService $shortenedUrlService)
+    {
+        $this->shortenedUrlService = $shortenedUrlService;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $inputs = $request->all();
+        $validator = Validator::make(
+            $inputs,
+            [
+                'original_url' => 'required|url',
+                'code_url'     => 'nullable|unique:shortened_urls,code_url',
+                'expiration'   => 'nullable|date_format:Y-m-d'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        return $this->shortenedUrlService->store($inputs);
+    }
+}
---
+++ app/Service/ShortenedUrlService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\ShortenedUrl;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class ShortenedUrlService
+{
+    /**
+     * @param array $inputs
+     * @return string
+     */
+    public function store(array $inputs)
+    {
+        try {
+            $inputs['code_url'] = $inputs['code_url'] ?? $this->getCodeUrl();
+            $inputs['expiration'] = $inputs['expiration'] ?? $this->getExpirationDate();
+
+            ShortenedUrl::create($inputs);
+
+            return response()->json(['shortened_url' => url("/{$inputs['code_url']}")]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Não foi possível encurtar a url.'
+            ], 500);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getCodeUrl(): string
+    {
+        do {
+            $codeUrl = Str::random(5);
+            $codeUrlExists = ShortenedUrl::where('code_url', $codeUrl)->exists();
+        } while ($codeUrlExists);
+
+        return $codeUrl;
+    }
+
+    /**
+     * @param integer $days
+     * @return string
+     */
+    private function getExpirationDate(int $days = 7): string
+    {
+        return Carbon::now()->addDays($days)->format('Y-m-d');
+    }
+}
