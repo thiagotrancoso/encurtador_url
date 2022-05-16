d5ae917843c003cd656e11ffcd7239b51b93cad7
Thiago Trancoso <thiagostrn@gmail.com>
2022-05-16 15:46:45 -0300

    Added redirect to original url

---
+++ app/Http/Controllers/ShortenedController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShortenedUrl;
+use Carbon\Carbon;
+
+class ShortenedController extends Controller
+{
+    public function redirect($codeUrl)
+    {
+        $shortenedUrl = ShortenedUrl::where('code_url', $codeUrl)
+            ->where('expiration', '>', Carbon::now()->format('Y-m-d'))
+            ->first();
+
+        if (!$shortenedUrl) {
+            return "404";
+        }
+
+        return redirect($shortenedUrl->original_url);
+    }
+}
