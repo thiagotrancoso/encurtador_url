09ff87921ff48374b70f9a7c886efc06b096144a
Thiago Trancoso <thiagostrn@gmail.com>
2022-05-17 10:11:14 -0300

    Added helper convert date

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 if (!function_exists('menu_active')) {
@@ -18,5 +19,18 @@ if (!function_exists('menu_active')) {
         }
 
         return $return;
+    }
+}
+
+if (!function_exists('convert_date_br_to_db')) {
+    /**
+     * Converts the date to brazil format to database format.
+     *
+     * @param array|string $routeName
+     * @return string
+     */
+    function convert_date_br_to_db(string $date): string
+    {
+        return Carbon::createFromFormat('d/m/Y', $date)->format('Y-m-d');
     }
 }
