e000a4e9298ec34b8657870baf318521ce1efb4e
Thiago Trancoso <thiagostrn@gmail.com>
2022-05-17 10:48:41 -0300

    Improved database query

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\ShortenedUrl;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
     public function allLinks()
     {
-        $links = ShortenedUrl::paginate(10);
+        $links = ShortenedUrl::where('expiration', '>', Carbon::now()->format('Y-m-d'))->paginate(10);
 
         return view('admin.all-links', [
             'links' => $links
